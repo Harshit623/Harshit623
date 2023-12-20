858e4e33c003fef79428789ee9ee2d318a9ebca9
harshit upadhyay <108094373+Harshit623@users.noreply.github.com>
2023-12-20 20:18:46 +0530

    Add files via upload

---
+++ sorted.py
@@ -0,0 +1,28 @@
+# fruits = ['grapes', 'mango', 'apple']
+# fruits.sort()
+# print(fruits)
+
+# print("a+b")
+# r = lambda a : a+15
+# print(r(25))
+
+# r1 =lambda b: b*45
+# print(r1(42513364201452145312351))
+
+# double the opreator 
+# r = lambda a : a*15
+# print(r(2))
+
+# # triple the numreator
+# r1 = lambda b : b*15
+# print(r1(3))
+
+# # quadraple the numerator
+# r2 = lambda c : c*15
+# print(r2(4))
+# in this user if the opreator if user enter the 1*15 = 15 , 2*15 
+num = int(input("enter the number "))
+num1 = lambda a : a*15 
+# for i in range(0,12):
+#     print("good")
+print(num1(num))
